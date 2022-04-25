0664604d78f4739281b86ed877f7655f574b14c4
syu99 <ufcmjohan@gmail.com>
2022-04-25 08:47:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const bubble = document.createElement("span");// création d'un élément html en js
+bubble.classList.add("bubble");// injection du css
+document.body.appendChild(bubble);// injection de l'élément précédemment crée en js dans le html.
+
+const size = Math.random() * 200 + 100 + "px";
+bubble.style.height = size;
+bubble.style.width = size;
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+bubble.style.setProperty("--left", Math.random() * 100 + "%");
